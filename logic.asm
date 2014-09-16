a6c710a1becf2a7664a69217a4ef7f525e37fcb1
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2014-09-16 11:01:02 -0700

    TurnLeft, TurnRight, TurnAround.

--- logic.asm
+++ logic.asm
@@ -124,7 +124,176 @@ FindSomething: ;0x58
     retf
 
 ;0xB0
+TurnLeft:
+    ;    x  y  ->  x  y
+    ; W -1  0  ->  0  1  S
+    ; N  0 -1  -> -1  0  W
+    ; S  0  1  ->  1  0  E
+    ; E  1  0  ->  0 -1  N
 
-INCBIN "chips.exe", 0x6200+$, 0x2a70 - 0xB0
+    %stacksize small
+    %arg x:word, y:word, xOut:word, yOut:word
+    mov ax,ds
+    nop
+    inc bp
+    push bp
+    mov bp,sp
+    push ds
+    mov ds,ax
+    sub sp,byte +0x2
+
+    ; ax = x<<1 + y
+    mov ax,[x]
+    shl ax,1
+    add ax,[y]
+
+    inc ax
+    inc ax
+    jz .west ; -2
+    dec ax
+    jz .north ; -1
+    dec ax
+    dec ax
+    jz .south ; +1
+    dec ax
+    jz .east ; +2
+    jmp short .end
+    nop
+.west:
+    mov bx,[xOut]
+    mov word [bx],0
+    mov bx,[yOut]
+    mov word [bx],1
+    jmp short .end
+.north:
+    mov bx,[xOut]
+    mov word [bx],-1
+.setYOutToZero:
+    mov bx,[yOut]
+    mov word [bx],0
+    jmp short .end
+.south:
+    mov bx,[xOut]
+    mov word [bx],1
+    jmp short .setYOutToZero
+    nop
+.east:
+    mov bx,[xOut]
+    mov word [bx],0
+    mov bx,[yOut]
+    mov word [bx],-1
+.end:
+    lea sp,[bp-0x2]
+    pop ds
+    pop bp
+    dec bp
+    retf
+    nop
+
+; 116
+TurnRight:
+    ;    x  y  ->  x  y
+    ; W -1  0  ->  0 -1  N
+    ; N  0 -1  ->  1  0  E
+    ; S  0  1  -> -1  0  W
+    ; E  1  0  ->  0  1  S
+
+    %stacksize small
+    %arg x:word, y:word, xOut:word, yOut:word
+    mov ax,ds
+    nop
+    inc bp
+    push bp
+    mov bp,sp
+    push ds
+    mov ds,ax
+    sub sp,byte +0x2
+
+    mov ax,[x]
+    shl ax,1
+    add ax,[y]
+    inc ax
+    inc ax
+    jz .west
+    dec ax
+    jz .north
+    dec ax
+    dec ax
+    jz .south
+    dec ax
+    jz .east
+    jmp short .end
+    nop
+.west:
+    mov bx,[xOut]
+    mov word [bx],0
+    mov bx,[yOut]
+    mov word [bx],-1
+    jmp short .end
+.north:
+    mov bx,[xOut]
+    mov word [bx],1
+.setYOutToZero:
+    mov bx,[yOut]
+    mov word [bx],0
+    jmp short .end
+.south:
+    mov bx,[xOut]
+    mov word [bx],-1
+    jmp short .setYOutToZero
+    nop
+.east:
+    mov bx,[xOut]
+    mov word [bx],0
+    mov bx,[yOut]
+    mov word [bx],1
+.end:
+    lea sp,[bp-0x2]
+    pop ds
+    pop bp
+    dec bp
+    retf
+    nop
+
+;17c
+TurnAround:
+    ;    x  y  ->  x  y
+    ; W -1  0  ->  1  0  E
+    ; N  0 -1  ->  0  1  S
+    ; S  0  1  ->  0 -1  N
+    ; E  1  0  -> -1  0  W
+
+    %stacksize small
+    %arg x:word, y:word, xOut:word, yOut:word
+    mov ax,ds
+    nop
+    inc bp
+    push bp
+    mov bp,sp
+    push ds
+    mov ds,ax
+    sub sp,byte +0x2
+
+    ; *xOut = -x
+    mov ax,[x]
+    neg ax
+    mov bx,[xOut]
+    mov [bx],ax
+
+    ; *yOut = -y
+    mov ax,[y]
+    neg ax
+    mov bx,[yOut]
+    mov [bx],ax
+
+    lea sp,[bp-0x2]
+    pop ds
+    pop bp
+    dec bp
+    retf
+
+; 1a4
+
+INCBIN "chips.exe", 0x6200+$, 0x2a70 - 0x1a4
 
 ; vim: syntax=nasm
