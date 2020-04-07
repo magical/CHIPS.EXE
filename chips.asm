63200f01a22d62b260c42520dafca605450687b2
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2020-04-07 14:53:37 -0700

    add nes tileset

--- chips.asm
+++ chips.asm
@@ -361,7 +361,8 @@ VERSION:
 
 ; d800
 OBJ32_4:
-    INCBIN "res/nesmasked.bmp", 14
+    ;INCBIN "res/nesmasked.bmp", 14
+    INCBIN "res/mscc nes mask_optimized.bmp", 14
     TIMES 0x22 db 0xFF ; ???
     ALIGN SectorSize, db 0
 
