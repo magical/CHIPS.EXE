11b5d01e6e00010a66853ece7b6c33a20673a461
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2020-11-21 15:10:56 -0800

    Simplify extern declarations
    NASM's manual claims that, quote, "if a variable is declared both
    `GLOBAL' and `EXTERN' [...] it will be treated as `GLOBAL'". Which is
    half-true. If you declare it global and then extern, that works -
    the extern is ignored. But if you declare it extern and then global you
    get an error. So that's useless.
    
    Fortunately there's a third option: if you declare something EXTERN and
    then later give it a definition, it will be upgraded to GLOBAL. And of
    course multiple EXTERNs are fine. Which gives us exactly what we need.
    We can declare all functions EXTERN up front and not even worry about
    GLOBAL.

--- extern.sh
+++ extern.sh
@@ -2,10 +2,8 @@ set -eu
 i=2
 for file in seg2.asm logic.asm seg4.asm seg5.asm seg6.asm movement.asm sound.asm digits.asm; do
     echo "; $file"
-    echo "%if SEGMENT_NUMBER != $i"
     sed -E 's/^(\w+):$/func \1/' <$file |
     awk -e '/^; [0-9a-f]+$/ { addr=$2; } /^func / { printf("EXTERN %-30s; %d:%s\n", $2, '$i', addr); }'
-    echo "%endif"
     echo
     i=$((i+1))
 done
