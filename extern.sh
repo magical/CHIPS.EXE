101c21a99f5d6907644c9368fc728f405a220aa8
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2020-11-22 15:00:28 -0800

    extern.sh: remove dead code
    Obsolete; we use a different method to declare windows symbols
    (see `make headers`).

--- extern.sh
+++ extern.sh
@@ -7,9 +7,3 @@ for file in crt.asm seg2.asm logic.asm seg4.asm seg5.asm seg6.asm movement.asm s
     echo
     i=$((i+1))
 done
-#for file in info/*.sym; do
-#    echo "; $file"
-#    module=$(basename "$file" .sym)
-#    awk -e '/^[0-9]+ \w+/ { if ($2 == "equate" || $2 == "varargs") { sym=$3 } else { sym=$2 }; printf("EXTERN %-30s; @%d\n", "'"$module"'." sym, $1) }' <$file
-#    echo
-#done
